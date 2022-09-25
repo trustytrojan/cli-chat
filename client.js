cb19d870ac4590cf03eec976b063c706953ef9c3
trustytrojan <tarbot24@hotmail.com>
2022-09-25 16:35:05 -0400

    a lot of work

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ const readline = require('readline')
 // const { existsSync, writeFileSync } = require('fs')
 const { stdin, stdout, stderr, exit, argv } = process
 
-if(argv.length < 4) {
+if(argv.length < 5) {
   console.log(`Usage: <program_name> <ip> <port> <username>      
     <ip>           Server IP address
     <port>         Server port
@@ -13,7 +13,7 @@ if(argv.length < 4) {
 
 const host = argv[2]
 const port = Number.parseInt(argv[3])
-let name = argv[4]
+let username = argv[4]
 const server_address = `${host}:${port}`
 
 // add known_servers.json file parsing
@@ -38,6 +38,9 @@ socket.on('data', (data) => {
   timeout = setTimeout(timedOut, 5_000)
   const str = data.toString()
   if(str === '\0') return
+  if(str.startsWith('/')) {
+
+  }
   rl.pause()
   stdout.write('\n')
   resetCursor()
@@ -46,15 +49,20 @@ socket.on('data', (data) => {
   rl.resume()
 })
 
-console.log(`Connecting to server at ${server_address}`)
+const server_cmds = {
+  
+}
+
+console.log(`<Client> Press Ctrl+C to disconnect.`)
+console.log(`<Client> Connecting to server at ${server_address}`)
 timeout = setTimeout(timedOut, 5_000)
 
 socket.connect(port, host, async () => {
   clearTimeout(timeout)
-  socket.write(`/on_join name="${name}"`)
-  console.log(`You're connected. Say hi!`)
+  socket.write(`/on_join username="${username}"`)
+  console.log(`<Client> You're connected. Say hi!`)
   while(true) {
-    const str = await prompt(`[${name}]> `)
+    const str = await prompt(`[${username}]> `)
     resetCursor()
     if(str.length === 0) continue
     socket.write(str)
--- server.js
+++ server.js
@@ -1,99 +1,146 @@
-const { Server } = require('net')
+const { Server, Socket } = require('net')
 
 const port = 8080
 
-const clients = new Map()
+const addr_socket = new Map()
+const username_socket = new Map()
 
 setInterval(() => {
-  for(const client of clients.values())
-    client.write('\0')
+  for(const socket of addr_socket.values())
+    socket.write('\0')
 }, 4_000)
 
 const server = new Server((socket) => {
-  const client = new ChatClient(socket)
-  clients.set(client.address, client)
+  socket.addr = `${socket.remoteAddress}:${socket.remotePort}`
+  console.log(`[${socket.addr}] has connected`)
+  addr_socket.set(socket.addr, socket)
+
+  socket.on('data', (data) => {
+    const str = data.toString()
+    console.log(`Incoming data from [${socket.addr}]: \`${str}\``)
+    if(str.startsWith('/')) {
+      const args = str.split(' ')
+      const command = commands[args.shift().slice(1)]
+      if(!command) {
+        socket.write(`{Whisper from <Server>} Unknown command. Send "/help" to see all available commands.`)
+        return
+      }
+      command(args)
+      return
+    }
+    send_all(`[${socket.username}] ${str}`)
+  })
+
+  const commands = {
+    on_join([ username ]) {
+      if(!/^username="(.+)"$/.test(username)) {
+        whisper_stoc(socket, 'Your username field is empty. You will be disconnected. Please reconnect with a proper username.')
+        disconnect_client(socket)
+        return
+      }
+      socket.username = username.substring(1+username.indexOf('"'), username.lastIndexOf('"'))
+      username_socket.set(socket.username, socket)
+      welcome(socket.username)
+    },
+    on_leave() {
+      announce(`[${socket.username}] has disconnected :(`)
+      disconnect_client(socket)
+    },
+    help() {
+      whisper_stoc(socket, 'List of all commands below (type without quotes):')
+      whisper_stoc(socket, '"/help" Displays this message.')
+      whisper_stoc(socket, '"/ch_name <new_name>" Changes your username to <new_name>.')
+      whisper_stoc(socket, '"/whisper <[username]> <message>" Send a private message.')
+    },
+    ch_name([ new_username ]) {
+      if(!new_username) {
+        whisper_stoc(socket, 'Incorrect usage of "/ch_name".')
+        whisper_stoc(socket, 'Usage: /ch_name <new_username>')
+        return
+      }
+      announce(`[${socket.username}] has changed their name to [${new_username}]`)
+      username_socket.delete(socket.username)
+      socket.username = new_username
+      username_socket.set(socket.username, socket)
+    },
+    whisper([ recipient_name, ...message ]) {
+      function usage() {
+        whisper_stoc(socket, 'whisper: Incorrect usage.')
+        whisper_stoc(socket, 'whisper: Usage: /whisper <username> <message>')
+      }
+      if(!recipient_name) { usage(); return }
+      message = message.join(' ')
+      if(message.length === 0) { usage(); return }
+      const recipient_sock = username_socket.get(recipient_name)
+      if(!recipient_sock) {
+        whisper_stoc(socket, 'whisper: The specified user does not exist in this server.')
+        return
+      }
+      whisper_ctoc(socket, recipient_sock, message)
+    }
+  }
+
+  socket.on('end', commands.on_leave)
+
+  socket.on('error', (err) => {
+    console.error(`Error involving client ${socket.addr}!`)
+    console.error(err)
+    socket.destroy()
+    commands.on_leave()
+  })
 })
+
+process.on('SIGINT', close)
 
 server.listen(port)
 
 console.log(`Server started on port ${port}`)
 
-function announce(message) {
-  const announcement = `<Server> ${message}`
-  console.log(announcement)
-  for(const client of clients.values())
-    client.write(announcement)
+/**
+ * Whisper a message from the server to a client.
+ * @param {Socket} socket 
+ * @param {string} msg 
+ */
+function whisper_stoc(socket, msg) {
+  socket.write(`{From <Server>} ${msg}`)
 }
 
-class ChatClient {
-  commands = {
-    help: () => {
-      socket.write(`
-{Whisper from <Server>} List of all commands below:
-{Whisper from <Server>} /help\tDisplays this message
-{Whisper from <Server>} /leave\tDisconnects from the server
-{Whisper from <Server>} /name <new_name>\tChange your username to <new_name>`)
-    },
-    name: (args) => {
-      const new_name = args.join(' ')
-      announce(`[${this.name}] has changed their name to [${new_name}]`)
-      this.name = new_name
-    },
-    whisper: (args) => {
+/**
+ * Pass a whisper message to its recipient
+ * @param {Socket} sock_from 
+ * @param {Socket} sock_to 
+ * @param {string} msg 
+ */
+function whisper_ctoc(sock_from, sock_to, msg) {
+  sock_from.write(`{To [${sock_to.username}]} ${msg}`)
+  sock_to.write(`{From [${sock_from.username}]} ${msg}`)
+}
 
-    }
+function disconnect_client(socket) {
+  console.log(`Destroyed connection with [${socket.addr}]`)
+  addr_socket.delete(socket.addr)
+  username_socket.delete(socket.username)
+  socket.destroy()
+}
+
+function welcome(name) {
+  send_all(`<Server:Welcome> [${name}] has connected to the server. Say hi!`)
+}
+
+function announce(message) {
+  send_all(`<Server:Announcement> ${message}`)
+}
+
+function send_all(str) {
+  for(const socket of addr_socket.values())
+    socket.write(str)
+}
+
+function close() {
+  for(const socket of addr_socket.values()) {
+    socket.write(`<Server:Shutdown> This server is shutting down. You will be disconnected.`)
+    socket.destroy()
   }
-
-  // Properties
-  name;
-
-  /**
-   * Construct a ChatClient object.
-   * @param {Socket} socket 
-   */
-  constructor(socket) {
-    this.address = `${socket.remoteAddress}:${socket.remotePort}`
-
-    socket.on('data', (data) => {
-      const str = data.toString()
-      console.log(`[${this.name}]@${this.address} ${str}`)
-      if(str.startsWith('/')) {
-        const args = str.split(' ')
-        const command = this.commands[args.shift().slice(1)]
-        if(!command) {
-          
-        }
-        return
-      }
-      for(const client of clients.values())
-        client.write(`[${this.name}] ${str}`)
-    })
-
-    socket.on('end', ChatClient.commands.on_leave)
-
-    socket.on('error', (err) => {
-      console.error(`Error involving client ${address}!`)
-      console.error(err)
-      socket.destroy()
-      clients.delete(address)
-      console.error(`Destroyed connection with ${address}`)
-    })
-
-    this.socket = socket
-  }
-
-  on_join(args) {
-    const n = args[0] // MAKE THIS OBJECT ORIENTED
-    this.name = n.substring(1+n.indexOf('"'), n.lastIndexOf('"'))
-    announce(`[${this.name}] has connected to the server. Say hi!`)
-  }
-
-  on_leave() {
-    announce(`[${this.name}] has disconnected :(`)
-    clients.delete(address)
-  },
-
-  whisper(from, msg) {
-    this.socket.write(`{Whisper from ${from}} ${msg}`)
-  }
+  server.close()
+  process.exit(0)
 }
